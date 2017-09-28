9a2e672df396353be96a9485819d1ce2422e9513
PictorIgnotus <pictorignotusbm@gmail.com>
2017-09-28 15:15:11 +0200

    welcome view finished

--- SpacingGame/SpacingGame/View/SGView.Designer.cs
+++ SpacingGame/SpacingGame/View/SGView.Designer.cs
@@ -28,36 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SGView));
             this.gameTablePanel = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.welcomePictureBox = new System.Windows.Forms.PictureBox();
             this.newGameButton = new System.Windows.Forms.Button();
             this.pauseButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
             this.gameTablePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.welcomePictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // gameTablePanel
             // 
             this.gameTablePanel.BackColor = System.Drawing.Color.Black;
-            this.gameTablePanel.Controls.Add(this.button2);
+            this.gameTablePanel.Controls.Add(this.welcomeLabel);
+            this.gameTablePanel.Controls.Add(this.welcomePictureBox);
             this.gameTablePanel.Location = new System.Drawing.Point(39, 12);
             this.gameTablePanel.MaximumSize = new System.Drawing.Size(506, 500);
             this.gameTablePanel.MinimumSize = new System.Drawing.Size(506, 500);
             this.gameTablePanel.Name = "gameTablePanel";
             this.gameTablePanel.Size = new System.Drawing.Size(506, 500);
             this.gameTablePanel.TabIndex = 0;
+
+            #region Welcome 
             // 
-            // button2
+            // welcomeLabel
             // 
-            this.button2.BackColor = System.Drawing.Color.Black;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Image = global::SpacingGame.Properties.Resources.spaceship_23x23;
-            this.button2.Location = new System.Drawing.Point(216, 477);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(23, 23);
-            this.button2.TabIndex = 3;
-            this.button2.UseVisualStyleBackColor = false;
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.welcomeLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.welcomeLabel.Font = new System.Drawing.Font("Candara", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.welcomeLabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.welcomeLabel.Location = new System.Drawing.Point(105, 2);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(305, 59);
+            this.welcomeLabel.TabIndex = 0;
+            this.welcomeLabel.Text = "Spacing Game";
+            // 
+            // welcomePictureBox
+            // 
+            this.welcomePictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.welcomePictureBox.Image = global::SpacingGame.Properties.Resources.asteroidfield_399x433;
+            this.welcomePictureBox.InitialImage = global::SpacingGame.Properties.Resources.asteroidfield_399x433;
+            this.welcomePictureBox.Location = new System.Drawing.Point(53, 64);
+            this.welcomePictureBox.Name = "welcomePictureBox";
+            this.welcomePictureBox.Size = new System.Drawing.Size(399, 433);
+            this.welcomePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.welcomePictureBox.TabIndex = 1;
+            this.welcomePictureBox.TabStop = false;
+            #endregion
+
+            #region MainpageButtons
             // 
             // newGameButton
             // 
@@ -67,6 +90,7 @@
             this.newGameButton.TabIndex = 0;
             this.newGameButton.Text = "New Game";
             this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.NewGameButton_Clicked);
             // 
             // pauseButton
             // 
@@ -85,6 +109,9 @@
             this.exitButton.TabIndex = 2;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Clicked);
+            #endregion
+
             // 
             // SGView
             // 
@@ -101,6 +128,8 @@
             this.Name = "SGView";
             this.Text = "Spacing Game";
             this.gameTablePanel.ResumeLayout(false);
+            this.gameTablePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.welcomePictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -111,7 +140,8 @@
         private System.Windows.Forms.Button newGameButton;
         private System.Windows.Forms.Button pauseButton;
         private System.Windows.Forms.Button exitButton;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.PictureBox welcomePictureBox;
     }
 }
 
--- SpacingGame/SpacingGame/View/SGView.cs
+++ SpacingGame/SpacingGame/View/SGView.cs
@@ -12,9 +12,23 @@ namespace SpacingGame
 {
     public partial class SGView : Form
     {
+        private bool firstGame;
+
         public SGView()
         {
             InitializeComponent();
+            firstGame = true;
+        }
+
+        private void NewGameButton_Clicked(object sender, EventArgs e)
+        {
+            this.gameTablePanel.Controls.Remove(welcomeLabel);
+            this.gameTablePanel.Controls.Remove(welcomePictureBox);
+        }
+
+        private void ExitButton_Clicked(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
