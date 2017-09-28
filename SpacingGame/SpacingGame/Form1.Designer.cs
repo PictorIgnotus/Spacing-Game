f106777c16c818376a6295edf9937464776202b1
PictorIgnotus <pictorignotusbm@gmail.com>
2017-09-28 09:56:15 +0200

    default view

---
+++ SpacingGame/SpacingGame/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SpacingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.newGame = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // newGame
+            // 
+            this.newGame.Location = new System.Drawing.Point(42, 526);
+            this.newGame.Name = "newGame";
+            this.newGame.Size = new System.Drawing.Size(187, 23);
+            this.newGame.TabIndex = 0;
+            this.newGame.Text = "New Game";
+            this.newGame.UseVisualStyleBackColor = true;
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Location = new System.Drawing.Point(355, 526);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(187, 23);
+            this.pauseButton.TabIndex = 1;
+            this.pauseButton.Text = "Pause/Start";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(42, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Size = new System.Drawing.Size(500, 500);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.newGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button newGame;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
